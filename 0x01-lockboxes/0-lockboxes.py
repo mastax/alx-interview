9a348597c8347780ff13fee02634e02c4183e5ed
El Mostafa El Bahtouri <elel-bah@e1r6p4.1337.ma>
2024-10-10 19:51:07 +0100

    the problem solveddddsss

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,16 +10,17 @@ def can_unlock_all(boxes):
     based on keys that can be attained.
     Solution to the lockboxes problem
     """
-    unlocked = set()  # To keep track of unlocked boxes
-    to_unlock = [0]   # Start with box 0, which is unlocked
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-    while to_unlock:  # While there are boxes to unlock
-        box = to_unlock.pop()  # Get a box to unlock
-        if box not in unlocked:  # Check if it's already unlocked
-            unlocked.add(box)  # Mark it as unlocked
-            for key in boxes[box]:  # Use keys in this box
-                if key not in unlocked and key < len(boxes):
-                    to_unlock.append(key)  # Add it to the list to unlock later
-
-    return len(unlocked) == len(boxes)  # Check if all boxes are unlocked
-
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
