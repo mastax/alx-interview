629cc61fb9b0c7a079eed8dd73c3a35623662204
El Mostafa El Bahtouri <elel-bah@e1r6p4.1337.ma>
2024-10-15 20:05:35 +0100

    fix the problem dd

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,17 @@
 
 def minOperations(n):
     """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
+    if n <= 1:
+        return 0  # If n is 1 or less, no operations are needed or possible.
+
+    operations = 0
+    factor = 2  # Start checking for factors from 2
+
+    while n > 1:
+        if n % factor == 0:  # If factor is a divisor of n
+            operations += factor  # We perform a Copy and factor-1 pastes
+            n //= factor  # Reduce n by this factor
         else:
-            op += 1
-            body += next
-    if len(body) != n:
-        return 0
-    return op
+            factor += 1  # Move to the next possible factor
+
+    return operations
