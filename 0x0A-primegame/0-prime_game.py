b4ad5cb5e5fa8b115246c4bd9e311ebbe4a655ec
El Mostafa El Bahtouri <elel-bah@e3r1p4.1337.ma>
2024-12-12 21:39:30 +0100

    hello

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
-
+"""Module defining isWinner function for Prime Game."""
 
 def is_prime(n):
-    """Returns True if n is prime, else False."""
+    """
+    Check if a number is prime
+    Args:
+        n (int): Number to check for primality.
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
     if n < 2:
         return False
     for i in range(2, int(n ** 0.5) + 1):
@@ -11,77 +16,81 @@ def is_prime(n):
             return False
     return True
 
-
 def sieve_of_eratosthenes(n):
     """
-    Efficiently generate primes up to n using Sieve of Eratosthenes
+    Generate prime numbers up to n using Sieve of Eratosthenes.
+    Args:
+        n (int): Upper limit for prime number generation.
+    Returns:
+        list: List of prime numbers up to n.
     """
-    # Create a boolean array "is_prime[0..n]" and initialize 
-    # all entries it as true. A value in is_prime[i] will 
-    # finally be false if i is Not a prime, else true.
     is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
-    
-    for i in range(2, int(n**0.5) + 1):
+
+    for i in range(2, int(n ** 0.5) + 1):
         if is_prime[i]:
-            # Update all multiples of i starting from i*i
-            for j in range(i*i, n+1, i):
+            for j in range(i * i, n + 1, i):
                 is_prime[j] = False
-    
-    # Collect all prime numbers
-    return [i for i in range(2, n+1) if is_prime[i]]
+
+    return [i for i in range(2, n + 1) if is_prime[i]]
 
 
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
+    """
+    Determine the winner of the Prime Game.
+    Args:
+        x (int): Number of rounds to play.
+        nums (list): List of integers representing n for each round.
+    Returns:
+        str or None: Name of the winner, or None if no clear winner.
+    """
     if not nums or x <= 0:
         return None
 
-    mariaWinsCount = 0
-    benWinsCount = 0
-
+    maria_wins = 0
+    ben_wins = 0
     for n in nums:
         # Skip rounds where no game can be played
         if n < 2:
-            benWinsCount += 1
+            ben_wins += 1
             continue
 
         # Precompute primes up to n
         primes = sieve_of_eratosthenes(n)
-        
+
         # Simulate the game
-        currentSet = set(range(1, n + 1))
-        currentPlayer = 0  # 0 for Maria, 1 for Ben
+        current_set = set(range(1, n + 1))
+        current_player = 0  # 0 for Maria, 1 for Ben
 
         # Continue while primes are available in the current set
         while True:
             # Find valid primes in the current set
-            validPrimes = [p for p in primes if p in currentSet]
-            
+            valid_primes = [p for p in primes if p in current_set]
+
             # If no primes available, current player loses
-            if not validPrimes:
+            if not valid_primes:
                 break
-            
+
             # Choose smallest valid prime
-            prime = min(validPrimes)
-            
+            prime = min(valid_primes)
+
             # Remove prime and its multiples
-            currentSet = {x for x in currentSet if x % prime != 0}
+            current_set = {x for x in current_set if x % prime != 0}
             primes.remove(prime)
-            
+
             # Switch players
-            currentPlayer = 1 - currentPlayer
+            current_player = 1 - current_player
 
         # If no valid moves left and it was Maria's turn, Ben wins
-        if currentPlayer == 0:
-            benWinsCount += 1
+        if current_player == 0:
+            ben_wins += 1
         else:
-            mariaWinsCount += 1
+            maria_wins += 1
 
     # Determine overall winner
-    if mariaWinsCount > benWinsCount:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif benWinsCount > mariaWinsCount:
+    elif ben_wins > maria_wins:
         return "Ben"
-    
+
     return None
